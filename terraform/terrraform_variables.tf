513cf832f9a40874ac0772467ad428c2aa64a611
ist1112890 <noramviken@tecnico.ulisboa.pt>
2024-10-23 13:04:34 +0100

    terraform_variables

---
+++ terraform/terrraform_variables.tf
@@ -0,0 +1,31 @@
+
+964 B
+# How to define variables in terraform:
+# https://www.terraform.io/docs/configuration/variables.html
+# ID of the project, find it in the GCP console when clicking 
+# on the project name (on the top dropdown)
+
+variable "GCP_PROJECT_ID" {
+    default = "agisit-2425-website-g26"
+}
+# A list of machine types is found at:
+# https://cloud.google.com/compute/docs/machine-types
+# prices are defined per region, before choosing an instance
+# check the cost at: https://cloud.google.com/compute/pricing#machinetype
+# Minimum required is N1 type = "n1-standard-1, 1 vCPU, 3.75 GB RAM"
+
+variable "GCP_MACHINE_TYPE" {
+    default = "n1-standard-1"
+}
+# Regions list is found at:
+# https://cloud.google.com/compute/docs/regions-zones/regions-zones?hl=en_US
+# For prices of your deployment check:
+# Compute Engine dashboard -> VM instances -> Zone
+
+variable "GCP_ZONE" {
+    default = "europe-west4-b"
+}
+# Minimum required
+variable "DISK_SIZE" {
+    default = "15"
+}
