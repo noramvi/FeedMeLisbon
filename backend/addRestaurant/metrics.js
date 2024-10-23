2edcc7ad45e476bb08487f4a04aa0ba50fc1575c
ist1112828 <anna.mjolstad-svendsen@tecnico.ulisboa.pt>
2024-10-23 17:08:16 +0100

    fixup

--- backend/addRestaurant/metrics.js
+++ backend/addRestaurant/metrics.js
@@ -13,6 +13,8 @@ const httpRequestCount = new promClient.Counter({
   labelNames: ['method', 'route']
 });
 
+register.registerMetric(httpRequestCount);
+
 // Histogram for request duration (latency)
 const httpRequestDurationSeconds = new promClient.Histogram({
   name: 'http_request_duration_seconds',
@@ -21,6 +23,8 @@ const httpRequestDurationSeconds = new promClient.Histogram({
   buckets: [0.1, 0.5, 1, 2, 5]
 });
 
+register.registerMetric(httpRequestDurationSeconds);
+
 // Counter for HTTP errors
 const httpErrorCount = new promClient.Counter({
   name: 'http_error_count_total',
@@ -28,11 +32,15 @@ const httpErrorCount = new promClient.Counter({
   labelNames: ['method', 'route', 'status_code']
 });
 
+register.registerMetric(httpErrorCount);
+
 // Gauge for active connections
 const activeConnections = new promClient.Gauge({
   name: 'active_connections',
   help: 'Number of active connections'
 });
+
+register.registerMetric(activeConnections);
 
 // Histogram for database query duration
 const dbQueryDuration = new promClient.Histogram({
@@ -40,6 +48,8 @@ const dbQueryDuration = new promClient.Histogram({
   help: 'Duration of database queries in seconds',
   labelNames: ['operation']
 });
+
+register.registerMetric(dbQueryDuration);
 
 // Export all the metrics
 module.exports = {
